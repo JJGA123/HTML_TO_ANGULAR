77f01ed2c0d864037a3daa28430c3a27356e735e
Jhon Galvis <jhon.galvis@norsoft.com.co>
2020-01-15 23:38:45 -0500

    Creando y ajustando index con header, body y footer.

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
