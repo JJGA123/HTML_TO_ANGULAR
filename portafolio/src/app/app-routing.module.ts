0a5ace9b67c9f79df57a9a338ec1af4afa17999f
Jhon Galvis <jhon.galvis@norsoft.com.co>
2020-01-21 00:10:44 -0500

    Iniciando modulo 4.

--- portafolio/src/app/app-routing.module.ts
+++ portafolio/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot( routes , { useHash : true }) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
